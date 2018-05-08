84c0695f431de123afac3e46223c5c402bce3c34
Johnson  Ekedum <johnekedum@gmail.com>
2018-05-08 00:06:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,116 @@
 
 import dash
+import numpy as np
+from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
 import plotly.graph_objs as go
+import pandas as pd
+
+
+daily_df = pd.read_csv("https://raw.githubusercontent.com/jdpersona/Dash_Apps/master/daily_df.csv")
 
 
 app = dash.Dash(__name__)
 server = app.server
 
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    '5d1ea79569ed194d432e56108a04d188/raw/' +
-    'a9f9e8076b837d541398e999dcbac2b2826a81f8/'+
-    'gdp-life-exp-2007.csv')
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/dZVMbK.css'})
 
 
-app.layout = html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure={
-            'data': [
-                go.Scatter(
-                    x=df[df['continent'] == i]['gdp per capita'],
-                    y=df[df['continent'] == i]['life expectancy'],
-                    text=df[df['continent'] == i]['country'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df.continent.unique()
-            ],
-            'layout': go.Layout(
-                xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                yaxis={'title': 'Life Expectancy'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
+app.layout = html.Div(className='container', children=[
+    html.H1('NPS Changes Overtime'),
+    html.Hr(),
+    
+   html.Div(className='two columns', children=[ 
+   dcc.Dropdown(id='items',
+     options=[
+        {'label': '30 Day', 'value': 30},
+        {'label': '60 Day', 'value': 60},
+        {'label': '90 Day', 'value': 90}
+        ],
+         value = 30
+         ), 
+    ], style = {'width' : '28%'}),
+
+
+    html.Div( dcc.Graph(id='graph'), className='ten columns' )
+
 ])
+
+
+
+
+@app.callback(Output('graph', 'figure'), [Input('items', 'value')])    
+def update_graph(items):
+    window1 = daily_df.rolling(items).sum().dropna()
+   # Compute NPS
+    window1['total'] = window1.detractors + window1.passives + window1.promoters
+    window1['NPS'] = 100*((window1.promoters/window1.total) - (window1.detractors/window1.total))
+    window1['DetPct'] = window1.detractors/window1.total
+    window1['ProPct'] = window1.promoters/window1.total
+    window1['SE_NPS'] = 100*np.sqrt((window1.ProPct*(1-window1.ProPct) + window1.DetPct*(1-window1.DetPct) + 2*window1.ProPct*window1.DetPct)/window1.total)                 
+    window1['MoE'] = 1.96*window1.SE_NPS
+    window1['NPS_low'] = window1.NPS - window1.MoE
+    window1['NPS_high'] = window1.NPS + window1.MoE
+  # Plot 10-day NPS with MoE boundaries\
+   # Lists to use in plot
+    NPS = list(window1.NPS)
+    NPS_low = list(window1.NPS_low)
+    NPS_high = list(window1.NPS_high)
+    
+    upper_bound = go.Scatter(
+        name='Upper Bound',
+        x=window1.date,
+        y=window1.NPS_high,
+        mode='lines',
+        marker=dict(color="444"),
+        line=dict(width=0),
+        fillcolor='rgba(68, 68, 68, 0.3)',
+        fill='tonexty' )
+
+    trace = go.Scatter(
+      name='Measurement',
+      x=window1.date,
+      y=window1.NPS,
+      mode='lines',
+      line=dict(color='rgb(31, 119, 180)'),
+      fillcolor='rgba(68, 68, 68, 0.3)',
+      fill='tonexty')
+    
+    
+    lower_bound = go.Scatter(
+     name='Lower Bound',
+     x=window1.date,
+     y=window1.NPS_low,
+     marker=dict(color="444"),
+     line=dict(width=0),
+     mode='lines' )
+
+    # Trace order can be important
+      # with continuous error bars
+    data = [lower_bound, trace, upper_bound]
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            yaxis={'title': 'Daily NPS Score'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10}
+            )
+
+            }
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True, threaded=True)
     
-
-
+    
+    
+    
+    
+    
+    
+    
+    
     
     
